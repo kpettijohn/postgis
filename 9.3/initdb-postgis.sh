40721bb12970dce77f126755b2a3c2998e8bfb0a
Kevin Pettijohn <KPettijohn@esri.com>
2016-03-21 10:01:53 -0700

    Removed a few unnecessary commands

--- 9.3/initdb-postgis.sh
+++ 9.3/initdb-postgis.sh
@@ -4,8 +4,6 @@ set -x
 
 WAIT=${WAIT:-3}
 
-gosu postgres postgres -E -c listen_addresses='' &
-
 sleep $WAIT
 
 gosu postgres psql <<EOSQL
@@ -15,5 +13,3 @@ UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
 CREATE EXTENSION postgis;
 CREATE EXTENSION postgis_topology;
 EOSQL
-
-gosu postgres /usr/lib/postgresql/$PG_MAJOR/bin/pg_ctl stop
